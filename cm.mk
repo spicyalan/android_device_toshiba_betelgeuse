ccc33f9ab5a4e19cb3807d73764aa24fe0f5cabe
Artem Makhutov <artem@makhutov.org>
2013-03-09 23:51:54 +0100

    Added wingray:4.1.1 build fingerprint

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=betelgeuse BUILD_FINGERPRINT="motorola/tervigon/wingray:4.0.4/IMM76/292727:user/release-keys" PRIVATE_BUILD_DESC="tervigon-user 4.0.4 IMM76 292727 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=betelgeuse BUILD_FINGERPRINT="motorola/tervigon/wingray:4.1.1/JRO03H/405518:user/release-keys" PRIVATE_BUILD_DESC="tervigon-user 4.1.1 JRO03H 405518 release-keys"
 
 PRODUCT_NAME := cm_betelgeuse
 PRODUCT_RELEASE_NAME := betelgeuse
